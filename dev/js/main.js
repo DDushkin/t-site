7b32bbb6dd761477757b81659339e5ad8e2024f6
Drew Dushkin <minuswant@gmail.com>
2017-10-25 14:26:47 +0300

    2 страница фильтр

--- dev/js/main.js
+++ dev/js/main.js
@@ -13,5 +13,23 @@ $(document).ready(function(){
   });
   $(".fotorama__arr--next").appendTo(".fotorama__nav__shaft");
   $(".fotorama__arr--prev").prependTo(".fotorama__nav__shaft");
+
+
+
+  //toggle filters page 2
+  $("div[data-toggle]").click(function(e) {
+    e.preventDefault();  // prevent navigating
+    var selector = $(this).data("toggle");  // get corresponding element
+    if( $(this).hasClass("toggle-active") ) {  // если уже нажата, то скрыть
+      $(selector).hide();
+      $(this).removeClass("toggle-active");
+    }  else {                                 // если не нажата, то очистить все а потом показать нужное
+      $("div.filter-wrapper").hide();
+      $("div.filter__item").removeClass("toggle-active");
+      $(selector).show();
+      $(this).addClass("toggle-active");
+    }
+  });
+
+  
 });
- 

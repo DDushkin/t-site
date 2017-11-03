5e2b144922cc84a5a5e24aaa9ec29986ef13f825
volodko <a.f1ghter.v@gmail.com>
2017-11-03 12:10:26 +0200

    01/general add script to inline svg from img tag

--- dev/js/main.js
+++ dev/js/main.js
@@ -97,4 +97,33 @@ $(document).ready(function(){
       $(this).addClass("toggle-active");
     }
   });
+
+
+
+  //replace svg img to inline
+  // https://medium.com/@asadalikanwal/controlling-svg-via-css-for-transitions-and-hover-effects-fcd9b48a4562
+  $(function() {
+    $('img.svg').each(function() {
+             var $img = $(this);
+             var imgID = $img.attr('id');
+             var imgClass = $img.attr('class');
+             var imgURL = $img.attr('src');
+    $.get(imgURL, function(data) {
+                 // Get the SVG tag, ignore the rest
+                 var $svg = $(data).find('svg');
+    // Add replaced image's ID to the new SVG
+                 if (typeof imgID !== 'undefined') {
+                     $svg = $svg.attr('id', imgID);
+                 }
+                 // Add replaced image's classes to the new SVG
+                 if (typeof imgClass !== 'undefined') {
+                     $svg = $svg.attr('class', imgClass + ' replaced-svg');
+                 }
+    // Remove any invalid XML tags as per http://validator.w3.org
+                 $svg = $svg.removeAttr('xmlns:a');
+    // Replace image with new SVG
+                 $img.replaceWith($svg);
+    }, 'xml');
+    });
+  });
 });  // eof doc.ready

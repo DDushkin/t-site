ca744c53f38b96103cdf83da1f72ba7300d7a13a
Drew Dushkin <minuswant@gmail.com>
2018-01-23 14:24:22 +0200

    bug fix + counter 15 ,14 pages + 21 timeline fix

--- dev/js/main.js
+++ dev/js/main.js
@@ -292,6 +292,26 @@ if(screen.width > 1023){
     });
   // eof pagenav tabs (31)
 
+//(14)
+  $(".incr-btn").on("click", function (e) {
+      var $button = $(this);
+      var oldValue = $button.parent().find('.quantity').val();
+      $button.parent().find('.incr-btn[data-action="decrease"]').removeClass('inactive');
+      if ($button.data('action') == "increase") {
+          var newVal = parseFloat(oldValue) + 1;
+      } else {
+          // Don't allow decrementing below 1
+          if (oldValue > 1) {
+              var newVal = parseFloat(oldValue) - 1;
+          } else {
+              newVal = 1;
+              $button.addClass('inactive');
+          }
+      }
+      $button.parent().find('.quantity').val(newVal);
+      e.preventDefault();
+  });
+//eof counter
 
 (function() {
   var contents = $('.service-terms__item-container');
